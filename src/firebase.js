d3bedafcc5cae928e2d964fbb7d348466ab27db6
blassstanciuc <117684665+blassstanciuc@users.noreply.github.com>
2023-11-28 16:51:30 -0300

    Update firebase.js
    comit2

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyBInhUSfyZaddkMWRsNsVzI3iLWopGihUU",
+  authDomain: "balneario-kai.firebaseapp.com",
+  projectId: "balneario-kai",
+  storageBucket: "balneario-kai.appspot.com",
+  messagingSenderId: "736477250851",
+  appId: "1:736477250851:web:728bfce1365a7cbf1eb896",
+  measurementId: "G-JTR39PELE1"
 };
 
 export const app = initializeApp(firebaseConfig);
